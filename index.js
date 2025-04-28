d2c682e0d43951448c9b3d1393ac6d68944033a5
Johnny-Rocket <spaciousguide@gmail.com>
2025-04-28 18:44:37 -0500

    config file

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 async function fetchdata() {
+  let searchTerm = document.getElementById("type").value;
+  if (searchTerm == "") searchTerm = "70%27srock";
+  console.log(apiKey);
   let response = await fetch(
-    "https://api.giphy.com/v1/gifs/search?api_key=wIZWR7YBBHQ945d8rz5rq4wQ41cG3pZn&q=70%27s+rock&limit=35&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=35&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
   );
+
   let giphyJson = await response.json();
   let arrPictures = giphyJson.data;
 
   let container = document.getElementById("images");
+  container.innerHTML = "";
 
   for (let index = 0; index < arrPictures.length; index++) {
     let img = document.createElement("img");
@@ -14,5 +19,6 @@ async function fetchdata() {
     console.log(arrPictures[index].images.original.url);
   }
 }
-
 fetchdata();
+
+document.getElementById("Search").addEventListener("click", fetchdata);
