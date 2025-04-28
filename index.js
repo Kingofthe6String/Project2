2461556f886d22d567b7599f43d7561f71365471
Johnny-Rocket <spaciousguide@gmail.com>
2025-04-28 16:38:38 -0500

    async function without loop

---
+++ index.js
@@ -0,0 +1,12 @@
+async function fetchdata() {
+  let response = await fetch(
+    "https://api.giphy.com/v1/gifs/search?api_key=wIZWR7YBBHQ945d8rz5rq4wQ41cG3pZn&q=70%27s+rock&limit=35&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+  );
+  let giphyJson = await response.json();
+  let arrPictures = giphyJson.data;
+
+  for (let index = 0; index < arrPictures.length; index++)
+    console.log(arrPictures[index].images.original.url);
+}
+
+fetchdata();
